990a9029092b547292071c6ce6f275265c72de94
Wendy Chirinos <89660705+Wendy-Alejandra@users.noreply.github.com>
2024-01-15 17:16:01 -0500

    Estructura HTML y rendereizacion en view.js -HTML semántico actualizado -view.js li agregados

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import { example } from './dataFunctions.js';
 import { renderItems } from './view.js';
 
+
 import data from './data/dataset.js';
+const contenedor = document.querySelector("ul");
+// renderItems(data);
+contenedor.innerHTML= renderItems(data);
+
 
 console.log(example, renderItems(data), data);
--- src/view.js
+++ src/view.js
@@ -1,5 +1,19 @@
+
 export const renderItems = (data) => {
-  console.log(data)
+  // console.log(data)
   // Aquí comienza tu código y puedes retornar lo que tu necesites
-  return 'example';
+
+  const cruceros = document.querySelector('ul');
+  data.forEach(data => {
+    // console.log(data);
+    const li = document.createElement('li');
+    // const cruisePrice = document.createElement('p');
+    li.innerHTML = `${data.name} ${data.facts.departurePort}`;
+    cruceros.appendChild(li);
+    // cruisePrice.innerHTML = `${data.facts.cruisePrice}`;
+  });
+  
+  return data;
 };
+
+
