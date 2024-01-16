80faa3997255b3388c7f6413920a07160be9e26c
mcarrobo24 <mcaf.dev24@gmail.com>
2024-01-16 16:38:21 -0500

    Estilo de la pagina -Se crea el archivo css -Cambios en el view al dar una class="cards" a los li. -Se crea un div llamado main-container en el index. para darle estilos.

--- src/view.js
+++ src/view.js
@@ -5,8 +5,10 @@ export const renderItems = (data) => {
 
   const contenedorCruceros = document.createElement('ul');
   data.forEach(crucero => {
+    const li = document.createElement('li');
+
     const image = document.createElement('img');
-    const liName = document.createElement('li');
+    const dlName = document.createElement('dl');
     const ddPort = document.createElement('dd');
     const ddPrice = document.createElement('dd');
     const ddDescription = document.createElement('dd');
@@ -14,18 +16,21 @@ export const renderItems = (data) => {
     
     image.setAttribute('itemprop', 'image');
     image.setAttribute('src', `${crucero.imageUrl}`);
-    liName.setAttribute('itemscope', '');
-    liName.setAttribute('itemtype', 'name');
+    li.setAttribute('itemscope', '');
+    li.setAttribute('itemtype', 'name');
+    li.setAttribute('class', 'cards');
+    dlName.setAttribute('itemprop', 'name');
     ddPort.setAttribute('itemprop', 'departureBoatTerminal');
     ddPrice.setAttribute('itemprop', 'totalPrice');
     ddDescription.setAttribute('itemprop', 'description');
-    liName.innerHTML = `${crucero.name}`;
+
+    dlName.innerHTML = `${crucero.name}`;
     ddPort.innerHTML = `${crucero.facts.departurePort}`;
     ddPrice.innerHTML = `${crucero.facts.cruisePrice}`;
     ddDescription.innerHTML = `AVG PER PERSON`;
-    // ul.setAttribute('itemscope=')
-    liName.append(ddPort, ddPrice, ddDescription);
-    contenedorCruceros.append(image, liName); /*esto va en main.js*/
+ 
+    li.append(image, dlName, ddPort, ddPrice, ddDescription);
+    contenedorCruceros.append(li); /*esto va en main.js*/
 
   });  
   return contenedorCruceros;
